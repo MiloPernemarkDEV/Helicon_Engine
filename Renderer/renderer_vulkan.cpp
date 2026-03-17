efa6007a76c728896f12806b52a75d636788ba07
MiloPernemarkDEV <milo.pernemark@student.forsbergsskola.se>
2026-03-17 21:24:48 +0100

    started on the vulkan instance creation

--- Renderer/renderer_vulkan.cpp
+++ Renderer/renderer_vulkan.cpp
@@ -16,3 +16,23 @@ void renderer_draw_frame_vulkan() {
 
 
 }
+
+//
+// Internal implementations
+//
+void create_instance(VkInstance& instance)
+{
+	VkApplicationInfo app_info{};
+	app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	app_info.pApplicationName = "Game Title";
+	app_info.pEngineName = "Helicon";
+	app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0); 
+	app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	app_info.apiVersion = VK_MAKE_VERSION(1, 4, 335);
+
+	VkInstanceCreateInfo create_info{};
+	create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	create_info.pApplicationInfo = &app_info;
+
+	vkCreateInstance(&create_info, nullptr, &instance),
+}
--- Renderer/renderer_vulkan.h
+++ Renderer/renderer_vulkan.h
@@ -3,12 +3,14 @@
 #include <vulkan/vulkan.h>
 
 struct renderer_vulkan {
-
+	VkInstance vulkan_instance;
 };
 
+// Interface 
 bool renderer_initialize_vulkan();
 void renderer_shutdown_vulkan();
 void renderer_draw_frame_vulkan();
 
+// Internal
 
-
+void create_instance(VkInstance& vulkan_instance);
