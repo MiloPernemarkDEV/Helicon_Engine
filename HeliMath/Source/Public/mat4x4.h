96f8a39003894436efc389a15a8742c80ea24c38
MiloPernemarkDEV <milo.pernemark@student.forsbergsskola.se>
2026-04-29 16:47:26 +0200

    Add matrix default constructor and matrix float members

--- HeliMath/Source/Public/mat4x4.h
+++ HeliMath/Source/Public/mat4x4.h
@@ -1,5 +1,7 @@
 #pragma once 
 
+// A matrix is an array of vectors that is arragned into rows and columns 
+
 #include "math_defines.h"
 #include "vec3.h"
 #include <cmath>
@@ -7,7 +9,19 @@
 struct Mat4x4 {
 	// Column major for OpenGL 
 	// m[column][row] 
-	float m[16];
+	float m11, m12, m13, m14,
+		  m21, m22, m23, m24,
+		  m31, m32, m33, m34,
+		  m41, m42, m43, m44;
+
+	HELI_FORCE_INLINE Mat4x4() 
+		: m11(1.0f), m12(0.0f), m13(0.0f), m14(0.0f),
+		  m21(1.0f), m22(0.0f), m23(0.0f), m24(0.0f),
+		  m31(1.0f), m32(0.0f), m33(0.0f), m34(0.0f),
+		  m41(1.0f), m42(0.0f), m43(0.0f), m44(0.0f)
+	{
+
+	}
 
 	
 };
